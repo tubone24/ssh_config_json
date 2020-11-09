37515e83443c20fc3f9237a677aeda63a50d319b
tubone24 <tubo.yyyuuu@gmail.com>
2020-11-10 02:12:41 +0900

    :sparkles: encrypt class

---
+++ ssh_config_json/encrypt.py
@@ -0,0 +1,53 @@
+import base64
+import hashlib
+import string
+import os
+from random import SystemRandom
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
+
+class AESCipher(object):
+    def __init__(self, key=None, key_num=32):
+        if key is not None:
+            self.key = (hashlib.md5(key.encode('utf-8')).hexdigest()).encode('utf-8')
+        else:
+            raw_key = self.create_key(key_num)
+            print(f"Encrypt key: {raw_key}")
+            self.key = (hashlib.md5(raw_key.encode('utf-8')).hexdigest()).encode('utf-8')
+
+    @staticmethod
+    def create_key(key_num=32):
+        return ''.join([SystemRandom().choice(string.ascii_letters + string.digits) for i in range(key_num)])
+
+    def encrypt(self, raw_text):
+        initialization_vector = Random.get_random_bytes(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_EAX, initialization_vector)
+        data = Padding.pad(raw_text.encode('utf-8'), AES.block_size, 'pkcs7')
+        return base64.b64encode(initialization_vector + cipher.encrypt(data))
+
+    def decrypt(self, encrypted):
+        encrypted = base64.b64decode(encrypted)
+        iv = encrypted[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_EAX, iv)
+        data = Padding.unpad(cipher.decrypt(encrypted[AES.block_size:]), AES.block_size, 'pkcs7')
+        return data.decode('utf-8')
+
+    def encrypt_file(self, path, delete_raw_file=False):
+        with open(path, "r") as f1, open(path + ".enc", "wb") as f2:
+            f2.write(self.encrypt(f1.read()))
+        if delete_raw_file:
+            os.remove(path)
+
+    def decrypt_file(self, path, delete_raw_file=False):
+        with open(path, "rb") as f1, open(path.replace(".enc", ""), "w") as f2:
+            f2.write(self.decrypt(f1.read()))
+        if delete_raw_file:
+            os.remove(path)
+
+
+if __name__ == '__main__':
+    ci = AESCipher("test")
+    # ci.encrypt_file("requirements2.txt")
+    ci.decrypt_file("requirements2.txt.enc")
